027359f273d9c6541bdad0fbda9f4613b1c648f2
Simon Baumgartner <makesens3@gmail.com>
2011-06-25 21:46:51 -0400

    more logging for production

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.configure('development', function(){
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 // Routes
