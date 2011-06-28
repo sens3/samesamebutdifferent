701da7680b6f0ef47fc6b553e8aab14870eac379
Simon Baumgartner <makesens3@gmail.com>
2011-06-27 23:54:53 -0400

    one more err logging for mongo

--- cache.js
+++ cache.js
@@ -6,9 +6,11 @@ Cache = {
 	getImageUrl: function(city, source, onEnd){
 		console.log('Cache.getImageUrl from ' + source.name + ' for ' + city.name);
 		Image.find({cityname: city.name, source: source.name}, function(err, docs){
-			if (docs.length != 0)
+			if(err)
+				console.log(err);
+			if (docs.length != 0){
 				onEnd(Array.random(docs).url);
-			else{
+			}else{
 				source.getImages(Array.random(city.latLngs), city.name, function(images){
 					images.forEach(function(image){
 						Image.find({source_id: image.source_id}, function(err, docs){
